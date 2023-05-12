968e4b2da9fc9b97127c84ae6a54e8dadcab81de
Dawit Tadesse <dawittadesse927@gmail.com>
2023-05-12 21:15:42 +0300

    FileStorage: class doc

---
+++ models/engine/file_storage.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''Defines the FileStorage class.'''
+
+import json
+from models.base_model import BaseModel
+
+class FileStorage:
+    '''Definition of class FileStorage that handles serialization of instances
+       to JSON file and deserialization of JSON files to instances
+    Attributes:
+        __file_path (str): The name of the file to save objects to.
+        __objects (dict): A dictionary of instantiated objects.
+    '''
