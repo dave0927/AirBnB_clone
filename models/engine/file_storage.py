70492e9a716a4eb0cea758f427d67da4acbd279a
Dawit Tadesse <dawittadesse927@gmail.com>
2023-05-12 21:29:53 +0300

    FileStorage: private class attributes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,3 +11,6 @@ class FileStorage:
         __file_path (str): The name of the file to save objects to.
         __objects (dict): A dictionary of instantiated objects.
     '''
+
+    __file_path = "file.json"
+    __objects = {}
