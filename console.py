b811d2c34805839cc0609c4d28737e3d958fe23c
Dawit Tadesse <dawittadesse927@gmail.com>
2023-05-13 13:28:11 +0300

    Console: Creates a new instance of BaseModel and saves it to JSON file

--- console.py
+++ console.py
@@ -2,6 +2,9 @@
 '''The entry point of the command interpreter.'''
 
 import cmd
+import models
+from models.base_model import BaseModel
+
 
 class HBNBCommand(cmd.Cmd):
     '''HBNBCommand implements the Cmd class.
@@ -12,17 +15,38 @@ class HBNBCommand(cmd.Cmd):
     '''
     prompt = '(hbnb) '
 
-    def do_quit(self, line):
+    def do_quit(self):
         '''Quit command to exit the program.'''
         return True
 
-    def do_EOF(self, line):
+    def do_EOF(self):
         '''EOF signal to exit the program.'''
         return True
 
     def emptyline(self):
         '''Do nothing when receiving an empty line.'''
         pass
+
+    def do_create(self, line):
+        '''Creates a new instance of BaseModel, saves it to the JSON file
+            prints the id.
+            Usage: create <ClassName>
+        '''
+        cmds = line.split()
+
+        if len(cmds) == 0:
+            print("** class name missing **")
+            return
+
+        try:
+            eval(cmds[0])
+        except NameError:
+            print("** class doesn't exist **")
+            return
+
+        newModel = eval(cmds[0])()
+        print(newModel.id)
+        newModel.save()
 
 
 if __name__ == '__main__':
