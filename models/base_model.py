ec566ced6d5fa3d617e5b48ba4bda45bdb7ef3e4
amanabay <amanuelabay22@gmail.com>
2023-05-14 13:34:24 +0000

    Fix BaseModel complacency issu

--- models/base_model.py
+++ models/base_model.py
@@ -21,7 +21,9 @@ class BaseModel():
         '''
 
         time_format = '%Y-%m-%dT%H:%M:%S.%f'
-
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
         if kwargs:
             for key, value in kwargs.items():
                 if key == '__class__':
@@ -32,9 +34,6 @@ class BaseModel():
                 else:
                     setattr(self, key, value)
         else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
             models.storage.new(self)
 
     def __str__(self):
