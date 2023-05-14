aa309dd126e3ce8ca11ec9b2ad304c04cceb2441
amanabay <amanuelabay22@gmail.com>
2023-05-14 08:16:11 +0000

    FileStorage: Add support for remaining classes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,6 +4,10 @@
 import json
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
 
 
 class FileStorage:
@@ -52,4 +56,3 @@ class FileStorage:
                     self.new(eval(cls_name)(**o))
         except FileNotFoundError:
             return
-
